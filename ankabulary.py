63485164472fcb2b03f3145b11e6e99245852670
Michael Dwyer <mdwyer84+github@gmail.com>
2023-08-05 17:28:06 -0400

    Better logging and more robust error handling

--- ankabulary.py
+++ ankabulary.py
@@ -2,8 +2,11 @@ import requests
 import json
 
 api_key = '4996b4ec-219f-411e-9d36-403e40db7a7d'
-input_file_name = 'word_list.txt'
-output_file_name = 'anki_cards.txt'
+input_file_name = 'raw_word_list.txt'
+output_file_name = 'generated_anki_cards.txt'
+total_processed_words = 0
+total_skipped_words = 0
+skipped_words_list = []
 
 
 class ProcessedWord:
@@ -15,9 +18,19 @@ class ProcessedWord:
 
 
 def main():
+    global total_skipped_words
+    global total_processed_words
+    global skipped_words_list
+
+    print('Connecting to Merriam-Webster.')
     words = fetch_definitions_from_file()
     write_anki_file(words)
-    print("Ankabulary execution complete!")
+
+    print('Ankabulary execution complete! ')
+    print('*****************************************************************')
+    print(f'{total_processed_words} words processed and written to {output_file_name}')
+    print(f'{total_skipped_words} words skipped {skipped_words_list}')
+    print('Never forget that you are loved <3')
 
 
 def fetch_definitions_from_file():
@@ -39,22 +52,27 @@ def fetch_definitions_from_file():
 
 
 def fetch_single_word(word):
+    global total_skipped_words
+    global skipped_words_list
+
     processed_word = ProcessedWord()
 
     url = f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}'
     response_obj = requests.post(url, json={})
     response_txt = response_obj.text
     response_dict = None
+    top_definition = None
     try:
         response_array = json.loads(response_txt)
         response_dict = response_array[0]
+        top_definition = response_dict['def'][0]['sseq'][0][0][1]['dt'][0][1]
     except Exception as e:
         print(
-            f'Error when looking up word "{word}" in Merriam-Webster. \n'
-            f'Response: {response_txt}. \nSkipping Word.')
-        return response_dict
+            f'Error when looking up word "{word}". Skipping Word.')
+        total_skipped_words = total_skipped_words + 1
+        skipped_words_list.append(word)
+        return None
 
-    top_definition = response_dict['def'][0]['sseq'][0][0][1]['dt'][0][1]
     processed_word.definition = clean_webster_formatting(top_definition)
     processed_word.part_of_speech = clean_webster_formatting(
         response_dict['fl'])
@@ -64,13 +82,9 @@ def fetch_single_word(word):
             response_dict['et'][0][1])
     except Exception as e:
         print(
-            f'Info: Word "{word}" has no etymology in Merriam-Webster.')
+            f'Info: Word "{word}" has no etymology.')
 
-    multi_entry_delimiter = ":"
-    word_id = response_dict['meta']['id']
-    if multi_entry_delimiter in word_id:
-        word_id = word_id[:word_id.index(multi_entry_delimiter)]
-    processed_word.word = word_id
+    processed_word.word = clean_word_id(response_dict['meta']['id'])
 
     return processed_word
 
@@ -91,14 +105,16 @@ def write_anki_file(words):
         write_word(word, output_file)
 
     output_file.close()
-    print(f'Processed words written to {output_file_name}')
 
 
 def write_word(word: ProcessedWord, file):
+    global total_processed_words
+
     file.write(word.word + "\t")
     file.write(word.definition + "\t\t\t\t\t")
     file.write(word.part_of_speech + "\t\t\t")
     file.write(word.etymology + "\n")
+    total_processed_words = total_processed_words + 1
 
 
 def clean_webster_formatting(input):
@@ -107,8 +123,10 @@ def clean_webster_formatting(input):
     result = remove_synonym_wrappers(result)
     result = remove_auto_link_wrappers(result)
     result = remove_direct_link_wrappers(result)
-    result = remove_italics_wrappers(result)
+    result = remove_etymology_link_wrappers(result)
+    result = remove_directional_etymology_chunk(result)
     result = remove_more_at_chunk(result)
+    result = replace_italics_wrappers(result)
     return result
 
 
@@ -118,7 +136,7 @@ def remove_wrapper(input, wrapper_start, wrapper_end):
     result = input
     start_idx = result.index(wrapper_start)
     end_idx = result.index(wrapper_end, start_idx + len(wrapper_start))
-    inner_word = result[start_idx + len(wrapper_start):end_idx]
+    inner_word = clean_word_id(result[start_idx + len(wrapper_start):end_idx])
     result = result[0:start_idx] + \
         inner_word + result[end_idx + len(wrapper_end):]
 
@@ -162,17 +180,21 @@ def remove_direct_link_wrappers(input):
     return input
 
 
+def remove_etymology_link_wrappers(input):
+    wrapper_start = '{et_link|'
+    while wrapper_start in input:
+        chunk_start_idx = input.index(
+            wrapper_start) + len(wrapper_start)
+        wrapper_end = generate_wrapped_chunk(input[chunk_start_idx:], '|', '}')
+        input = remove_wrapper(input, wrapper_start, wrapper_end)
+
+    return input
+
+
 def remove_synonym_wrappers(input):
     synonym_start = '{sx|'
     while synonym_start in input:
         input = remove_wrapper(input, synonym_start, '||}')
-    return input
-
-
-def remove_italics_wrappers(input):
-    italics_start = '{it}'
-    while italics_start in input:
-        input = remove_wrapper(input, italics_start, '{/it}')
     return input
 
 
@@ -184,11 +206,32 @@ def remove_more_at_chunk(input):
     return input
 
 
+def remove_directional_etymology_chunk(input):
+    wrapper_start = '{dx_ety}'
+    while wrapper_start in input:
+        chunk = generate_wrapped_chunk(input, wrapper_start, '{/dx_ety}')
+        input = input.replace(chunk, '')
+    return input
+
+
+def clean_word_id(input):
+    multi_entry_delimiter = ":"
+    if multi_entry_delimiter in input:
+        input = input[:input.index(multi_entry_delimiter)]
+    return input
+
+
 def replace_colons(input):
     colon_str = '{bc}'
     if colon_str in input and input.index(colon_str) == 0:
         input = input[4:]
     return input.replace(colon_str, '; ')
+
+
+def replace_italics_wrappers(input):
+    input = input.replace('{it}', '<em>')
+    input = input.replace('{/it}', '</em>')
+    return input
 
 
 main()
