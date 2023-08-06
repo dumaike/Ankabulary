3c5a7883508063d4c7db5a636e2effc0adc812ac
Michael Dwyer <mdwyer84+github@gmail.com>
2023-08-06 16:25:37 -0400

    Added support for reformatting directional wrappers

--- ankabulary.py
+++ ankabulary.py
@@ -1,6 +1,10 @@
 import requests
 import json
 
+# ****************************************************************************#
+#                                 GLOBAL                                      #
+# ****************************************************************************#
+
 # Configuration
 api_key = '4996b4ec-219f-411e-9d36-403e40db7a7d'
 input_file_name = 'raw_word_list.txt'
@@ -10,8 +14,9 @@ space_char = '&nbsp;'
 # Global logging variables
 total_processed_words = 0
 total_not_found_words = 0
-total_skipped_words = 0
 not_found_words_list = []
+total_skipped_words = 0
+skipped_words_list = []
 
 
 class ProcessedWord:
@@ -34,14 +39,21 @@ def main():
     print('Ankabulary execution complete! ')
     print('*****************************************************************')
     print(f'{total_processed_words} words processed and written to {output_file_name}')
-    print(
-        f'Definitions for {total_not_found_words} words not found - {not_found_words_list}')
-    print(f'{total_skipped_words} duplicate words skipped')
+    if total_not_found_words > 0:
+        print(
+            f'Definitions for {total_not_found_words} words not found - {not_found_words_list}')
+    if total_skipped_words > 0:
+        print(f'{total_skipped_words} duplicate words skipped - {skipped_words_list}')
     print('Never forget that you are loved <3')
 
 
+# ****************************************************************************#
+#                       FETCHING AND RESPONSE READING                         #
+# ****************************************************************************#
+
 def fetch_definitions_from_file():
     global total_skipped_words
+    global skipped_words_list
 
     try:
         input_file = open(input_file_name, "r")
@@ -57,6 +69,7 @@ def fetch_definitions_from_file():
         # If this exact word already exists in the results, skip it.
         if word.lower() in processed_words:
             total_skipped_words = total_skipped_words + 1
+            skipped_words_list.append(word)
             continue
 
         proccessed_result = fetch_single_word(word)
@@ -90,10 +103,8 @@ def fetch_single_word(word):
 
         # Only take the first part of speech to proccess
         response_dict = response_array[0]
-        merged_definitions = read_definitions_from_response(
+        processed_word.definition = read_definitions_from_response(
             response_dict, word)
-        processed_word.definition = clean_webster_formatting(
-            merged_definitions)
     except Exception as e:
         print(
             f'Error: when looking up word "{word}". Skipping Word.')
@@ -120,20 +131,23 @@ def read_definitions_from_response(response_dict, word):
     return_definition = ''
     definitions = response_dict['def']
 
+    definition_index = 1
     for sub_def in definitions:
         sense_sequence = sub_def['sseq']
         for sub_sseq in sense_sequence:
             for sub_sseq2 in sub_sseq:
                 if 'sn' in sub_sseq2[1]:
                     return_definition = return_definition + \
-                        read_single_sense(sub_sseq2)
+                        read_single_sense(definition_index, sub_sseq2)
+                    definition_index = definition_index + 1
                 elif sub_sseq2[0] == 'pseq':
                     for pseq in sub_sseq2[1]:
                         return_definition = return_definition + \
-                            read_single_sense(pseq)
+                            read_single_sense(definition_index, pseq)
+                        definition_index = definition_index + 1
                 elif 'dt' in sub_sseq2[1]:
                     # This word only has a single definition, so just grab it.
-                    return sub_sseq2[1]['dt'][0][1]
+                    return clean_webster_formatting(sub_sseq2[1]['dt'][0][1])
                 else:
                     print(f'Error: The API Response for {word} had unexpected '
                           'formatting. Double check the card output.')
@@ -141,21 +155,16 @@ def read_definitions_from_response(response_dict, word):
     return return_definition
 
 
-def read_single_sense(sense):
-    sense_number = sense[1]['sn']
-    tokenized_sense_numbers = sense_number.split()
-    formatted_sense_numbers = ''
-    # TODO: Format these nicely with html spaces when a fixed width font
-    # is used in the card styling.
-    for token in tokenized_sense_numbers:
-        formatted_sense_numbers = formatted_sense_numbers + \
-            token + ' '
-
-    sense_text = sense[1]['dt'][0][1]
-    definition = formatted_sense_numbers + sense_text + '<br>'
+def read_single_sense(index, sense):
+    sense_text = clean_webster_formatting(sense[1]['dt'][0][1])
+    definition = str(index) + ') ' + sense_text + '<br>'
 
     return definition
 
+
+# ****************************************************************************#
+#                              OUTPUT GENERATION                              #
+# ****************************************************************************#
 
 def write_anki_file(words):
     # Exit early if there are no words to avoid false logging.
@@ -185,6 +194,10 @@ def write_word(word: ProcessedWord, file):
     total_processed_words = total_processed_words + 1
 
 
+# ****************************************************************************#
+#                              STRING FORMATTING                              #
+# ****************************************************************************#
+
 # The Webster formatting has a lot of custom markup that we need to strip,
 # convert to html, or change to special characters.
 def clean_webster_formatting(input):
@@ -194,6 +207,7 @@ def clean_webster_formatting(input):
     result = remove_auto_link_wrappers(result)
     result = remove_direct_link_wrappers(result)
     result = remove_etymology_link_wrappers(result)
+    result = remove_directional_wrappers(result)
     result = remove_directional_etymology_chunk(result)
     result = remove_more_at_chunk(result)
     result = replace_italics_wrappers(result)
@@ -215,7 +229,7 @@ def remove_wrapper(input, wrapper_start, wrapper_end):
 
 
 # A chunk is an entire wrapped section, including the inner contents.
-def generate_wrapped_chunk(input, chunk_start, chunk_end):
+def copy_wrapped_chunk(input, chunk_start, chunk_end):
     chunk_start_idx = 0
     chunk_end_idx = 0
     if chunk_start not in input:
@@ -224,13 +238,15 @@ def generate_wrapped_chunk(input, chunk_start, chunk_end):
     else:
         chunk_start_idx = input.index(chunk_start)
 
-    if chunk_end not in input:
+    # Just the substring from the chunk start to the end of the full input.
+    partial_chunk = input[chunk_start_idx + len(chunk_start):]
+    if chunk_end not in partial_chunk:
         print(f'Error: Could not find wrapper_end \
-              ({chunk_end}) in string ({input})')
+              ({chunk_end}) in string ({input}) after {chunk_start}')
     else:
-        chunk_end_idx = input.index(chunk_end)
+        chunk_end_idx = partial_chunk.index(chunk_end)
 
-    return input[chunk_start_idx: chunk_end_idx + len(chunk_end)]
+    return chunk_start + partial_chunk[: chunk_end_idx + len(chunk_end)]
 
 
 def remove_auto_link_wrappers(input):
@@ -245,7 +261,7 @@ def remove_direct_link_wrappers(input):
     while direct_link_start in input:
         chunk_start_idx = input.index(
             direct_link_start) + len(direct_link_start)
-        wrapper_end = generate_wrapped_chunk(input[chunk_start_idx:], '|', '}')
+        wrapper_end = copy_wrapped_chunk(input[chunk_start_idx:], '|', '}')
         input = remove_wrapper(input, direct_link_start, wrapper_end)
 
     return input
@@ -256,7 +272,7 @@ def remove_etymology_link_wrappers(input):
     while wrapper_start in input:
         chunk_start_idx = input.index(
             wrapper_start) + len(wrapper_start)
-        wrapper_end = generate_wrapped_chunk(input[chunk_start_idx:], '|', '}')
+        wrapper_end = copy_wrapped_chunk(input[chunk_start_idx:], '|', '}')
         input = remove_wrapper(input, wrapper_start, wrapper_end)
 
     return input
@@ -269,10 +285,30 @@ def remove_synonym_wrappers(input):
     return input
 
 
+def remove_directional_wrappers(input):
+    outer_wrapper_start = '{dx}'
+    outer_wrapper_end = '{/dx}'
+    inner_wrapper_start = '{dxt|'
+    while outer_wrapper_start in input:
+        original_outer_chunk = copy_wrapped_chunk(
+            input, outer_wrapper_start, outer_wrapper_end)
+        inner_chunk = copy_wrapped_chunk(
+            original_outer_chunk, inner_wrapper_start, '}')
+        inner_wrapper_end = copy_wrapped_chunk(
+            inner_chunk[len(inner_wrapper_start):], '|', '}')
+        outer_chunk_with_unwrapped_inner_word = remove_wrapper(
+            original_outer_chunk, inner_wrapper_start, inner_wrapper_end)
+        unwrapped_outer_chunk = remove_wrapper(
+            outer_chunk_with_unwrapped_inner_word, outer_wrapper_start, outer_wrapper_end)
+        input = input.replace(original_outer_chunk,
+                              '; ' + unwrapped_outer_chunk)
+    return input
+
+
 def remove_more_at_chunk(input):
     more_at_start = '{ma}'
     while more_at_start in input:
-        chunk = generate_wrapped_chunk(input, more_at_start, '{/ma}')
+        chunk = copy_wrapped_chunk(input, more_at_start, '{/ma}')
         input = input.replace(chunk, '')
     return input
 
@@ -280,7 +316,7 @@ def remove_more_at_chunk(input):
 def remove_directional_etymology_chunk(input):
     wrapper_start = '{dx_ety}'
     while wrapper_start in input:
-        chunk = generate_wrapped_chunk(input, wrapper_start, '{/dx_ety}')
+        chunk = copy_wrapped_chunk(input, wrapper_start, '{/dx_ety}')
         input = input.replace(chunk, '')
     return input
 
@@ -288,7 +324,7 @@ def remove_directional_etymology_chunk(input):
 def remove_cross_reference_chunk(input):
     wrapper_start = '{dx_def}'
     while wrapper_start in input:
-        chunk = generate_wrapped_chunk(input, wrapper_start, '{/dx_def}')
+        chunk = copy_wrapped_chunk(input, wrapper_start, '{/dx_def}')
         input = input.replace(chunk, '')
     return input
 
@@ -305,11 +341,9 @@ def clean_word_id(input):
 
 def replace_colons(input):
     colon_str = '{bc}'
-    # Each definition has a leading colon, which doesn't suit single definition
-    # Anki formatting so we used to strip that. Now that we're taking multiple
-    # definitions, we will include it.
-    # if colon_str in input and input.index(colon_str) == 0:
-    #    input = input[4:]
+    # Each definition has a leading colon, which doesn't suit Ank formatting
+    if colon_str in input and input.index(colon_str) == 0:
+        input = input[4:]
 
     # Replace the remaining colons.
     return input.replace(colon_str, '<b>:</b> ')
